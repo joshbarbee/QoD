3d4f59d72b987bc4fbb8c4f8acf8be8870af27b1
Josh Barbee <32991764+joshbarbee@users.noreply.github.com>
2017-12-24 15:27:24 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,40 +2,64 @@ import tkinter as tk
 from tkinter import *
 import requests
 import math
-root = tk.Tk()
-root.title("Quote of the Day")
-root.overrideredirect(True)
 
-root.lift()
-root.wm_attributes("-transparentcolor", "white")
-root.wm_attributes("-disabled", True)
-root.attributes('-alpha', 0.8)
-root.iconbitmap("icon.png")
-
-w = root.winfo_screenwidth()
-h = root.winfo_screenheight()
-
-link = requests.get('https://talaikis.com/api/quotes/random/')
-RESPONSE = link.json()['quote']   
-RESPONSE2 = link.json()['author']
-new = RESPONSE.split(" ")
-
-l = []
-b = []
-a = ""
-for i in range(0,len(new),5):
+def main():
     
-    l+=(new[i:i+5],''+'\n')
+    root = tk.Tk()
     
-for i in l:
-    a += ' '.join(i)
-m = a.split("\n")
+    top = Frame(root)
+    bottom = Frame(root)
 
-b.sort(reverse=True)
+    top.config(bg="lightgray")
+    top.pack(side=TOP)
 
-lolbel = Label(root, text = "Quote of the day"+"\n"+"\n"+ a+ "\n-" + RESPONSE2, height = math.floor(len(new)/5+5), width = 35,font=('Georgia','10'), bg='gray')
+    bottom.config(bg="gray")
+    bottom.pack(side=BOTTOM, fill=BOTH, expand=True)
+    root.title("Quote of the Day")
+    root.overrideredirect(True)
+    
+    root.lift()
+    root.wm_attributes("-transparentcolor", "white")
+    root.columnconfigure(0, weight=1)
+    root.rowconfigure(1, weight=1)
+    root.attributes('-alpha', 0.8)
+    root.iconbitmap("icon.png")
 
-lolbel.pack(side = tk.BOTTOM)
-root.geometry("+1630+5")
-root.smooth=1
-root.mainloop()
+    w = root.winfo_screenwidth()
+    h = root.winfo_screenheight()
+
+    link = requests.get('https://talaikis.com/api/quotes/random/')
+    RESPONSE = link.json()['quote']   
+    RESPONSE2 = link.json()['author']
+    new = RESPONSE.split(" ")
+
+    l = []
+    b = []
+    a = ""
+    for i in range(0,len(new),5):
+        
+        l+=(new[i:i+5],''+'\n')
+        
+    for i in l:
+        a += ' '.join(i)
+    m = a.split("\n")
+
+    b.sort(reverse=True) 
+    newl = Label(root,text = "Quotify v 0.05                                                              ", bg = None)
+    newl.pack(in_=top, side=LEFT)
+
+    b1 = Button(root,text = " X ",  command = root.destroy, bg = None)
+    b1.config(width = 1, height = 1, borderwidth = 0)
+    
+    b1.pack(in_=top, side=RIGHT)
+
+    lolbel = Label(root, text = "-       Quote of the day       -"+"\n"+"\n"+ a+ "\n-" + RESPONSE2, height = math.floor(len(new)/5+5), width = 35,font=('Georgia','10'), bg='gray')
+
+    lolbel.pack(in_=bottom, side = BOTTOM)
+    root.geometry("+1630+5")
+    
+    
+    root.mainloop()
+    
+if __name__==('__main__'):
+    main()
